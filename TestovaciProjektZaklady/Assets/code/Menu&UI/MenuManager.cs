4c80a0554cd2e074569f346dc9217bea8a87a788
UliasMichal <misa.uli.4.a.4@seznam.cz>
2022-01-04 17:56:23 +0100

    SaveSystem - backup
    Backup před stáhnutí NewtonJson package

---
+++ TestovaciProjektZaklady/Assets/code/Menu&UI/MenuManager.cs
@@ -0,0 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+//using Newtonsoft.json;
+
+public class MenuManager : MonoBehaviour
+{
+    //Ovládací objekty
+    public GameObject ObjektySimulace;
+    public GameObject TimeManager;
+
+    //Time
+    public GameObject TimeMenu;
+    public GameObject PauseButton;
+    public GameObject PlayButton;
+    
+    //ControlPanel
+    public GameObject ControlPanelCamera;
+    public GameObject ControlPanelObject;
+    public GameObject ControlPanelProbe;
+    public GameObject ControlPanelOptions;
+
+    //Pop-ups
+    public GameObject CloseWindow;
+    public GameObject AsteroidMenu;
+
+    public enum ControlPanelModes 
+    {
+        allHidden,
+        cameraCP,
+        objectCP,
+        probeCP,
+        optionsCP
+    }
+        
+
+    public void SpeedChange(bool zvysit)
+    {
+        if (zvysit)
+        {
+            Debug.Log("Speed increased");
+        }
+        else
+        {
+            Debug.Log("Speed decreased");
+        }
+    }
+
+    public void SpeedPausePlay(bool isPause)
+    {
+        if (isPause)
+        {
+            PlayButton.SetActive(true);
+            Debug.Log("Simulation paused");
+            PauseButton.SetActive(false);
+        }
+        else
+        {
+            PauseButton.SetActive(true);
+            Debug.Log("Simulation resumed");
+            PlayButton.SetActive(false);
+        }
+    }
+
+    public void ControlPanelChangeInt(int cpm)
+    {
+        //Z dùvodu Unity, parametr nemùže být enum
+        if (cpm == (int)ControlPanelModes.allHidden)
+        {
+            ControlPanelCamera.SetActive(false);
+            ControlPanelObject.SetActive(false);
+            ControlPanelProbe.SetActive(false);
+            ControlPanelOptions.SetActive(false);
+
+            return;
+        }
+        if (cpm == (int)ControlPanelModes.cameraCP)
+        {
+            ControlPanelCamera.SetActive(true);
+
+            ControlPanelObject.SetActive(false);
+            ControlPanelProbe.SetActive(false);
+            ControlPanelOptions.SetActive(false);
+
+            return;
+        }
+        if (cpm == (int)ControlPanelModes.objectCP)
+        {
+            ControlPanelObject.SetActive(true);
+
+            ControlPanelCamera.SetActive(false);
+            ControlPanelProbe.SetActive(false);
+            ControlPanelOptions.SetActive(false);
+
+            return;
+        }
+        if (cpm == (int)ControlPanelModes.probeCP)
+        {
+            ControlPanelProbe.SetActive(true);
+
+            ControlPanelCamera.SetActive(false);
+            ControlPanelObject.SetActive(false);
+            ControlPanelOptions.SetActive(false);
+
+            return;
+        }
+        if (cpm == (int)ControlPanelModes.optionsCP)
+        {
+            ControlPanelOptions.SetActive(true);
+
+            ControlPanelCamera.SetActive(false);
+            ControlPanelObject.SetActive(false);
+            ControlPanelProbe.SetActive(false);
+
+            return;
+        }
+    }
+
+    public void SaveToFile(int fileNumber) 
+    {
+        
+        string path = Application.persistentDataPath + "/LoadFile" + fileNumber.ToString() + ".slf";
+
+        SimulationData dataToSave = new SimulationData(ObjektySimulace, TimeManager.GetComponent<TimeManager>());
+        //string json = JsonUtility.ToJson(dataToSave);
+        /*
+        string json = JsonConvert.SerializeObject(account, Formatting.Indented);
+
+
+        StreamWriter sw = new StreamWriter(path);
+
+        sw.Write(json);
+
+        Debug.Log(path);
+        sw.Close();
+        */
+    }
+}
